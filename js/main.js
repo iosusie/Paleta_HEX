8e9079d7400284d167f95ab8209d8fbe3752f963
iosusie <iosu.zaldibar@gmail.com>
2022-03-23 16:15:16 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.getElementById('color').addEventListener('input',getColor)
+
+function getColor(){
+    let col = document.getElementById('color').value
+
+    document.getElementById('visualizar').style.background=col
+    document.getElementById('visualizar').innerHTML=col
+
+    document.getElementById('titulo').style.color=col
+}
